6706f0d66e1385306322047826034c35ef4a2ec5
Halon Hamilton <halon.j.hamilton@gmail.com>
2019-08-06 11:34:35 -0500

    addind quick bench

--- client/src/Home/Sidebar/SidebarComponents/Bench.js
+++ client/src/Home/Sidebar/SidebarComponents/Bench.js
@@ -1,89 +1,63 @@
-import React, { Fragment } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
+export default function SwitchLabels() {
+
+
+  const [state, setState] = React.useState({
+   Quaterback: { 
+     qb1: true,
+    qb2: false
   },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
+  Runninback:{
+    rb1: true,
+    rb2: false,
+    rb3: true,
+    rb4: false
   },
-}));
+   Widereciever:{
+     wr1: true,
+     wr2: false,
+     wr3: false,
+     wr4:false,
+     wr5: true,
+     wr6: true
+   }
+  
+  });
 
-export default function ControlledOpenSelect() {
-  const classes = useStyles();
-  const [age, setAge] = React.useState('');
-  const [open, setOpen] = React.useState(false);
+ 
 
-  function handleChange(event) {
-    setAge(event.target.value);
-  }
-
-  function handleClose() {
-    setOpen(false);
-  }
-
-  function handleOpen() {
-    setOpen(true);
-  }
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+  };
+ 
 
   return (
-      <Fragment>
-    <form autoComplete="off">
-      <Button className={classes.button} onClick={handleOpen}>
-        
-      </Button>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="demo-controlled-open-select">Quarter-back</InputLabel>
-        <Select
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={age}
-          onChange={handleChange}
-          inputProps={{
-            name: 'Quater-back',
-            id: 'demo-controlled-open-select',
-          }}
-        >
-         
-          <MenuItem value={1}>QB1</MenuItem>
-          <MenuItem value={2}>QB2</MenuItem>
-          
-        </Select>
-      </FormControl>   
-    </form>
-    <form autoComplete="off">
-      <Button className={classes.button} onClick={handleOpen}>
-        
-      </Button>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="demo-controlled-open-select">Quarter-back</InputLabel>
-        <Select
-          open={open}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          value={age}
-          onChange={handleChange}
-          inputProps={{
-            name: 'Quater-back',
-            id: 'demo-controlled-open-select',
-          }}
-        >
-         
-          <MenuItem value={1}>QB1</MenuItem>
-          <MenuItem value={2}>QB2</MenuItem>
-          
-        </Select>
-      </FormControl>   
-    </form>
-    </Fragment>
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={state.qb1}
+            onChange={handleChange("qb1")}
+            value="qb1"
+          />
+        }
+        label="Qb1"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            checked={state.qb2}
+            onChange={handleChange("qb2")}
+            value="qb2"
+            color="primary"
+          />
+        }
+        label="Qb2"
+      />
+    </FormGroup>
   );
 }
