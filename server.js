e30d72ccf37b20148b7b505ceffca3b1cc494e4a
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-08-08 20:38:14 -0500

    slight syntax changes, added city name to teams and header for box

--- server.js
+++ server.js
@@ -4,30 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // var db = require("./models"); - not being used right now. need to comment this back in later.
-///////////////////////////////////////////////////////////
-//For scraping the news 
-// const cheerio = require('cheerio')
-// const axios = require('axios')
 
-// axios.get('https://sports.yahoo.com/nfl/').then((response) => {
-//   const $ = cheerio.load(response.data)
-//   //THIS SELECTOR NEEDS TO BE TWEAKED THIS IS JUST THE BASED CODE
-//   const urlElems = $('li')
-    
-//   for (let i = 0; i < urlElems.length; i++) {
-//     const urlSpan = $(urlElems[i]).find('a')[0]
-
-//     if (urlSpan) {
-//       const urlText = $(urlSpan).text().slice('Yahoo Sports')
-//       // We then print the text on to the console
-//       console.log(urlText)
-//     }
-//     else(console.log("Didn't work"))
-//   }
-  
-// });
-
-///////////////////////////////////////////////////////////////
 
 var db = require("./models");
 
