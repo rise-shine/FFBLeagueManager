1fd172ddd71c04c6b41c12750cdd4812af44ab13
Jake Smith <jacobwsmith19@gmail.com>
2019-08-10 11:38:16 -0500

    Updated Roster.js file

--- client/src/Layouts/Roster.js
+++ client/src/Layouts/Roster.js
@@ -1,5 +1,6 @@
 import React from "react";
 import rosters from "../rosters.json";
+import '../Layouts/Roster.css';
 
 export default class Roster extends React.Component {
 
@@ -19,21 +20,21 @@ export default class Roster extends React.Component {
 
                     return (
                         <ul>
-                        {player.Team.name}
-                        <li>QB: {player.QB1.first} {player.QB1.last}</li>
-                        <li>QB: {player.QB2.first} {player.QB2.last}</li>
-                        <li>RB: {player.RB1.first} {player.RB1.last}</li>
-                        <li>RB: {player.RB2.first} {player.RB2.last}</li>
-                        <li>RB: {player.RB3.first} {player.RB3.last}</li>
-                        <li>RB: {player.RB4.first} {player.RB4.last}</li>
-                        <li>WR: {player.WR1.first} {player.WR1.last}</li>
-                        <li>WR: {player.WR2.first} {player.WR2.last}</li>
-                        <li>WR: {player.WR3.first} {player.WR3.last}</li>
-                        <li>WR: {player.WR4.first} {player.WR4.last}</li>
-                        <li>WR: {player.WR5.first} {player.WR5.last}</li>
-                        <li>WR: {player.WR6.first} {player.WR6.last}</li>
-                        <li>K: {player.KIC.first} {player.KIC.last}</li>
-                        <li>DEF: {player.DEF.city} {player.DEF.team}</li>
+                        <span class="rosterText">{player.Team.name} </span>
+                        <li><span class="rosterText">QB: </span>{player.QB1.first} {player.QB1.last}</li>
+                        <li><span class="rosterText">QB: </span>{player.QB2.first} {player.QB2.last}</li>
+                        <li><span class="rosterText">RB: </span>{player.RB1.first} {player.RB1.last}</li>
+                        <li><span class="rosterText">RB: </span>{player.RB2.first} {player.RB2.last}</li>
+                        <li><span class="rosterText">RB: </span>{player.RB3.first} {player.RB3.last}</li>
+                        <li><span class="rosterText">RB: </span>{player.RB4.first} {player.RB4.last}</li>
+                        <li><span class="rosterText">WR: </span>{player.WR1.first} {player.WR1.last}</li>
+                        <li><span class="rosterText">WR: </span>{player.WR2.first} {player.WR2.last}</li>
+                        <li><span class="rosterText">WR: </span>{player.WR3.first} {player.WR3.last}</li>
+                        <li><span class="rosterText">WR: </span>{player.WR4.first} {player.WR4.last}</li>
+                        <li><span class="rosterText">WR: </span>{player.WR5.first} {player.WR5.last}</li>
+                        <li><span class="rosterText">WR: </span>{player.WR6.first} {player.WR6.last}</li>
+                        <li><span class="rosterText">K: </span>{player.KIC.first} {player.KIC.last}</li>
+                        <li><span class="rosterText">DEF: </span>{player.DEF.city} {player.DEF.team}</li>
                         </ul>
                 )
                     
