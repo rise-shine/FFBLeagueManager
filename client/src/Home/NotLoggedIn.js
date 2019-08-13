e8b877dd3c6a6fe0430aa392dbde7c6c46d884c5
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-08-13 18:00:49 -0500

    added a page when the user is not logged in, although functionality is not yet complete

--- client/src/Home/NotLoggedIn.js
+++ client/src/Home/NotLoggedIn.js
@@ -1,28 +1,52 @@
-import React from "react";
-import News from "../Layouts/News/News";
-import { Paper } from "@material-ui/core";
-const style = {
-    Paper: { 
-    padding: 20,
-    margin: 10, 
-    maxHeight: 400,
-    maxWidth:8000,
-    textAlign:"center"
-    
-   }
-  };
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-function NotLoggedIn() {
-  return(
-  <container>
-    <Paper style={style.Paper}>
-      <h1>Click Here to Log In</h1>
-      <h2>Logging in allows you to access full features</h2>
-    </Paper>
-  <News />
-  </container>) 
-  
+export default function NotLoggedIn() {
+  const classes = useStyles();
 
+
+return (
+
+  <div className={classes.root}>
+
+    <Grid container spacing={3}>
+      <Grid item xs>
+      </Grid>
+      <Grid item xs={6}>
+        <Paper className={classes.paper}>
+          <h1>Please Log In</h1>
+          <h3>Logging in allows you to access more features</h3>
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required />
+            </div>
+           <a href="/xx"> <button type="button" class="btn btn-primary">Login</button> </a>
+
+          </form>
+
+        </Paper>
+      </Grid>
+      <Grid item xs>
+
+      </Grid>
+    </Grid>
+</div>
+    )
 }
-
-export default NotLoggedIn;
