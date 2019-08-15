9d583a1caa5b6032e5361a0d44fb24f1f073e9d2
Austxgolfer <taf442001@gmail.com>
2019-08-15 13:15:32 -0500

    updated controller and stat index files to improve api call routing.  updated stats.js so queries for player and defense are now working and saving to database.  queries will still need to be tweaked to specify week

--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
-const statsApi = require("./statsapi");
+const statsApi = require("./statsapi/stats");
 // Defining how Express will work with the API Routes
 router.use("/api", apiRoutes);
-router.use("/stats", statsApi);
+router.use("/statsapi", statsApi);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  res.send("statsapi route page");
 });
 
 module.exports = router;
